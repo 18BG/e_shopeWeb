e39b6817ae4378ded542e82539e046c45f49754b
Babry Galledou <babrygalledou@Babrys-MacBook-Pro.local>
2023-07-15 19:02:38 +0000

    first commit

---
+++ lib/helpers/responsiveness.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+const int largeScreenSize = 1366;
+const int customScreenSize = 110;
+const int mediumScreenSize = 768;
+const int smallScreenSize = 360;
+
+class ResponsiveWidget extends StatelessWidget {
+  final Widget largeScreen;
+  final Widget? mediumScreen;
+  final Widget smallScreen;
+  const ResponsiveWidget(this.mediumScreen, this.smallScreen,
+      {super.key, required this.largeScreen});
+
+  //small screen
+  static bool isSmallScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width < mediumScreenSize;
+
+  //medium screen
+  static bool isMediumScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width >= mediumScreenSize &&
+      MediaQuery.of(context).size.width < largeScreenSize;
+//custom screen
+  static bool isCustomScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width >= mediumScreenSize &&
+      MediaQuery.of(context).size.width <= largeScreenSize;
+//large screen
+  static bool isLargeScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width >= largeScreenSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      double width = constraints.maxWidth;
+      if (width >= largeScreenSize) {
+        return largeScreen;
+      } else if (width < largeScreenSize && width >= mediumScreenSize) {
+        return mediumScreen ?? largeScreen;
+      } else {
+        return smallScreen;
+      }
+    });
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.lightBlue,
+          child: Text("E - S H O P"),
+        ),
+      ),
+    );
+  }
+}
